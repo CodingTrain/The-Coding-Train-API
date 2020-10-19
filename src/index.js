fa02414961f9300c89405252fe3332d423d9bf7a
Fun Planet <dipamdiptam@gmail.com>
2020-10-19 16:50:16 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-const app = require('./app');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const api = require('./api');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use('/', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
