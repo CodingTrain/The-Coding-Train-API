97807c68d8700633c721ca469f40a28f45243fb4
Dipam Sen <funplanet.m4f@gmail.com>
2022-06-06 15:18:12 +0530

    update readme

--- README.md
+++ README.md
@@ -11,52 +11,36 @@ Response Type: `application/json`
 All endpoints are relative to [`https://the-coding-train-api.vercel.app`](https://the-coding-train-api.vercel.app)
 
 * `GET /` : Details about the API and other endpoints
-* `GET /:videoplaylist{/:videoIndex}` : Data about particular playlist (or video)
-* `GET /:videoplaylist/random` : Data about a random video in a playlist
+* `GET /challenge`: Details about Coding Challenges
+* `GET /challenge/:id`: Information about Coding Challenge
+* `GET /tracks/{:trackid}`: Details about Main Tracks
+* `GET /sidetracks/{:trackid}`: Details about Side Tracks
 
-| Actual Name                              | `videoplalist` Name |
-| ---------------------------------------- | ------------------- |
-| Coding Challenges                        | `challenge`         |
-| Coding in the Cabana                     | `cabana`            |
-| Code! Programming with p5.js             | `p5Tutorial`        |
-| Git and Github for Poets                 | `gitTutorial`       |
-| Working with Data and APIs in JavaScript | `dataapis`          |
-| The Nature of Code                       | `noc`               |
-| Teachable Machine Tutorials              | `teachableMachine`  |
-| Beginners Guide to Machine Learning      | `ml5`               |
-| Discord Bot                              | `discord`           |
-| Guest Tutorials                          | `guest`             |
 
-* `GET /challenge/randomContribution` : Random Contribution for a Coding Challenge
-* `GET /cabana/randomContribution` : Random Contribution for a Coding in the Cabana Video
-* `GET /ml5/randomContribution` : Random Community Contribution in Machine Learning with ml5.js 
-* `GET /noc/randomContribution` : Random Contribution based on The Nature of Code videos
-
-### Example Testing Routes
+<!-- ### Example Testing Routes
 **Open these links in the browser**
 * [/challenge/random](https://the-coding-train-api.vercel.app/challenge/random) : Get details about a Random Coding Challenge Video
 * [/discord](https://the-coding-train-api.vercel.app/discord) : Get details about the Discord Bot Tutorial Series
 * [/challenge/randomContribution](https://the-coding-train-api.vercel.app/challenge/randomContribution) : Get a random Community Contribution on a Coding Challenge Video
-* [/cabana/randomContribution](https://the-coding-train-api.vercel.app/cabana/randomContribution) : Get a random Community Contribution on a Coding in the Cabana Video
+* [/cabana/randomContribution](https://the-coding-train-api.vercel.app/cabana/randomContribution) : Get a random Community Contribution on a Coding in the Cabana Video -->
 
 
-## Usage
+<!-- ## Usage
 
 Here is an p5.js Web Editor Sketch which uses the API
-[The Coding Train API Example](https://editor.p5js.org/dipam2006/full/OV4TcmsxF)
+[The Coding Train API Example](https://editor.p5js.org/dipam2006/full/OV4TcmsxF) -->
 
 ## Contribution
-If any issue (eg. wrong response, etc) is found, please feel free to [create a new github issue](/issues).
+If any issue is found, please feel free to [create a new github issue](/issues).
 
 You can also contribute to the project by [opening a pull request.](/pulls)
 
 To test this API locally on your computer,
 * Clone this repo 
-  `git clone https://github.com/dipamsen/The-Coding-Train-API.git`
+  `git clone https://github.com/CodingTrain/The-Coding-Train-API.git`
 * Run `npm start` on the Terminal in the Project Directory
 * Open `localhost:4333` in the browser to use the API.
 
-(Go to `localhost:4333/challenge/randomContribution` for a random contribution)
 
 ## License
 MIT
--- src/functions.js
+++ src/functions.js
@@ -5,97 +5,115 @@ const baseURL = "https://thecodingtrain.com";
 
 const allPlaylists = {
   challenge: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_CodingChallenges",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_CodingChallenges",
     URLPrefix: `${baseURL}/CodingChallenges/`,
     title: "Coding Challenge",
-    description: "Watch Dan take on some viewer submitted Coding Challenges in p5.js and Processing!",
-    ytid: "PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH"
+    description:
+      "Watch Dan take on some viewer submitted Coding Challenges in p5.js and Processing!",
+    ytid: "PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH",
   },
   cabana: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_challenges/coding-in-the-cabana",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_challenges/coding-in-the-cabana",
     URLPrefix: `${baseURL}/challenges/coding-in-the-cabana/`,
     title: "Coding in the Cabana",
-    description: "The sun out is, the birds are chirping, it’s a beautiful day to code a generative algorithm. Choo choo!",
-    ytid: "PLRqwX-V7Uu6bVafiIHN-8LR3MUFDboJdU"
+    description:
+      "The sun out is, the birds are chirping, it’s a beautiful day to code a generative algorithm. Choo choo!",
+    ytid: "PLRqwX-V7Uu6bVafiIHN-8LR3MUFDboJdU",
   },
   p5Tutorial: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_beginners/p5js",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_beginners/p5js",
     URLPrefix: `${baseURL}/beginners/p5js/`,
     title: "Code! Programming in p5.js",
-    description: "This video series focuses on the fundamentals of computer programming (variables, conditionals, iteration, functions & objects) using JavaScript. In particular it leverages the p5.js creative computing environment which is oriented towards visual displays on desktops, laptops, tablets or smartphones. The series is designed for computer programming novices."
+    description:
+      "This video series focuses on the fundamentals of computer programming (variables, conditionals, iteration, functions & objects) using JavaScript. In particular it leverages the p5.js creative computing environment which is oriented towards visual displays on desktops, laptops, tablets or smartphones. The series is designed for computer programming novices.",
   },
   gitTutorial: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_beginners/git-and-github",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_beginners/git-and-github",
     URLPrefix: `${baseURL}/beginners/git-and-github/`,
     title: "Git and Github for poets",
-    description: "This video series is designed to teach you the basics of working with git version control and the GitHub website."
+    description:
+      "This video series is designed to teach you the basics of working with git version control and the GitHub website.",
   },
   dataapis: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_Courses/data-and-apis",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_Courses/data-and-apis",
     title: "Working with Data and APIs in JavaScript",
     URLPrefix: `${baseURL}/Courses/data-and-apis/`,
     description: "Welcome to Working with Data and APIs in Javascript!",
-    ytid: "PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X"
+    ytid: "PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X",
   },
   noc: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_learning/nature-of-code",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_learning/nature-of-code",
     URLPrefix: `${baseURL}/learning/nature-of-code/`,
     title: "The Nature of Code",
-    description: "These videos accompany The Nature of Code book."
+    description: "These videos accompany The Nature of Code book.",
   },
   guest: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_GuestTutorials",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_GuestTutorials",
     URLPrefix: `${baseURL}/GuestTutorials/`,
     title: "Guest Tutorials",
-    description: "Here you can find all tutorials made by guests on TheCodingTrain."
+    description:
+      "Here you can find all tutorials made by guests on TheCodingTrain.",
   },
   teachableMachine: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_TeachableMachine",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_TeachableMachine",
     URLPrefix: `${baseURL}/TeachableMachine/`,
     title: "The Teachable Machine",
-    description: "Introducing Teachable Machine 2.0 from Google Creative Lab! Train a computer to recognize your own images, sounds & poses.",
-    ytid: "PLRqwX-V7Uu6aJwX0rFP-7ccA6ivsPDsK5"
+    description:
+      "Introducing Teachable Machine 2.0 from Google Creative Lab! Train a computer to recognize your own images, sounds & poses.",
+    ytid: "PLRqwX-V7Uu6aJwX0rFP-7ccA6ivsPDsK5",
   },
   ml5: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_learning/ml5",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_learning/ml5",
     URLPrefix: `${baseURL}/learning/ml5/`,
     title: "Beginners Guide to Machine Learning in JavaScript",
     description: "ml5.js Beginners Guide",
-    ytid: "PLRqwX-V7Uu6YPSwT06y_AEYTqIwbeam3y"
+    ytid: "PLRqwX-V7Uu6YPSwT06y_AEYTqIwbeam3y",
   },
   discord: {
-    apiUrl: "https://api.github.com/repos/CodingTrain/website/contents/_learning/bots/discord",
+    apiUrl:
+      "https://api.github.com/repos/CodingTrain/website/contents/_learning/bots/discord",
     URLPrefix: `${baseURL}/learning/bots/discord/`,
     title: "Discord Bot",
     description: "🤖 How to create a Discord bot with discord.js! 🤖",
-    ytid: "PLRqwX-V7Uu6avBYxeBSwF48YhAnSn_sA4"
-  }
+    ytid: "PLRqwX-V7Uu6avBYxeBSwF48YhAnSn_sA4",
+  },
 };
 
-/** 
- * @type {<T>(arr:T[]) => T} 
+/**
+ * @type {<T>(arr:T[]) => T}
  * Get random element from an array
  * @param {Array<T>} arr Array
  */
-const randomArr = arr => arr[Math.floor(Math.random() * arr.length)];
+const randomArr = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 /**
  * Get details aboul all videos from a particular playlist
- * @param {string} playlist 
+ * @param {string} playlist
  * @returns {Promise<ArrayLike<{name: string, url: string, videoIndex: number}>>}
  */
 async function getAllVideos(playlist) {
   if (!allPlaylists[playlist]) return new Error("Playlist Not found");
   let url = allPlaylists[playlist].apiUrl;
-  let { data: challenges } = await axios.get(url, { auth: { username: "dipamsen" } });
-  challenges = challenges.filter(x => x.name !== "index.md");
+  let { data: challenges } = await axios.get(url, {
+    auth: { username: "dipamsen" },
+  });
+  challenges = challenges.filter((x) => x.name !== "index.md");
   let challengesWithIndex = {};
   for (let ch of challenges) {
     const index = +ch.name.match(/\d+[.\d]*/)[0];
     challengesWithIndex[index] = {
       name: ch.name.slice(0, -3),
       url: ch.download_url,
-      videoIndex: index
+      videoIndex: index,
     };
   }
   return challengesWithIndex;
@@ -112,7 +130,9 @@ async function getVideo(playlist, index) {
   try {
     const allVideos = await getAllVideos(playlist);
     let url = allVideos[index].url;
-    let { data: allVideoData } = await axios.get(url, { auth: { username: "dipamsen" } });
+    let { data: allVideoData } = await axios.get(url, {
+      auth: { username: "dipamsen" },
+    });
     let videoYaml = allVideoData.split("---")[1];
     let description = allVideoData.split("---").pop();
     let video = YAML.parse(videoYaml);
@@ -131,9 +151,12 @@ async function getVideo(playlist, index) {
       ...restVideoProperties
     } = video;
     let contributions;
-    if (restVideoProperties.contributions) contributions = restVideoProperties.contributions;
+    if (restVideoProperties.contributions)
+      contributions = restVideoProperties.contributions;
     else if (Math.floor(index) == index) contributions = [];
-    else contributions = (await getVideo(playlist, Math.floor(index) + 0.1)).contributions;
+    else
+      contributions = (await getVideo(playlist, Math.floor(index) + 0.1))
+        .contributions;
     return {
       ...restVideoProperties,
       videoIndex: video_number,
@@ -142,7 +165,7 @@ async function getVideo(playlist, index) {
       videoID: video_id,
       webEditor: web_editor,
       contributions,
-      playlist: allPlaylists[playlist].title
+      playlist: allPlaylists[playlist].title,
     };
   } catch (e) {
     return e;
@@ -160,23 +183,31 @@ const randomContribution = async (playlist, baseURL) => {
       index: video.videoIndex,
       url: video.webURL,
       apiUrl: baseURL + "/" + playlist + "/" + video.videoIndex,
-      series: allPlaylists[playlist].title
+      series: allPlaylists[playlist].title,
     };
-    if (video.contributions && typeof video.contributions == "object") return {
-      ...randomArr(video.contributions),
-      originalVideo: ogVideoData
-    };
-    else if (Math.floor(videoIndex) == videoIndex) return await randomContribution(playlist, baseURL);
+    if (video.contributions && typeof video.contributions == "object")
+      return {
+        ...randomArr(video.contributions),
+        originalVideo: ogVideoData,
+      };
+    else if (Math.floor(videoIndex) == videoIndex)
+      return await randomContribution(playlist, baseURL);
     else {
       const pt1 = await getVideo(playlist, Math.floor(videoIndex) + 0.1);
-      if (pt1.contributions) return {
-        ...randomArr(pt1.contributions),
-        originalVideo: ogVideoData
-      };
+      if (pt1.contributions)
+        return {
+          ...randomArr(pt1.contributions),
+          originalVideo: ogVideoData,
+        };
       else return await randomContribution(playlist, baseURL);
     }
   }
 };
 
-
-module.exports = { getAllVideos, getVideo, randomContribution, allPlaylists, randomArr };
+module.exports = {
+  getAllVideos,
+  getVideo,
+  randomContribution,
+  allPlaylists,
+  randomArr,
+};
